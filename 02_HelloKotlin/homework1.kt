99ebc3c92007dab214b82bf09d02531c0a1df0a6
Sergiu Neculita <sergiu.cahull.crihana@gmail.com>
2022-01-06 17:58:08 +0000

    I created a project in folder 02_HelloKotlin. I wrote the program and I used the print/println function and code comments

---
+++ 02_HelloKotlin/homework1.kt
@@ -0,0 +1,22 @@
+
+    fun main() {
+        print("Hey")
+        // print("hier is comment")
+        print(",")
+        print(" ")
+        print("my")
+        print(" ")
+        print("name")
+        print(" ")
+        print("is")
+        /* print("sergiu")
+     */
+        print(" ")
+        println("Sergiu")
+        println("I have no experience") /*I MEAN IT*/
+
+
+
+
+        println("hier is 2 println")
+    }
