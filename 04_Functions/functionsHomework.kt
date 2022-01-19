4c779cdc008afd9e806a11d761cbe0d756428148
sergiuneculita <199707adriana>
2022-01-19 20:25:00 +0100

    I created two functions that decipher the text

--- 04_Functions/functionsHomework.kt
+++ 04_Functions/functionsHomework.kt
@@ -4,9 +4,14 @@ fun main() {
     val text1 = text.take(12)  //"""F2p)v"y233{0"""
     //println(text1) //control first part
      val text2 = text.takeLast(12)
-    print(decodeText1(text1) + decodeText2(text2))
-    //println(text.map {(Char) -> text })
+    println(decodeText1(text1) + decodeText2(text2))
+    val textcoplet =( decodeText1(text1) + decodeText2(text2))
+    println(textcoplet)
+    //shift('d', 'a')
+   // println(shift('d','a' ))
+    println( textcoplet.map { char -> char +1  })
 }
+
 fun decodeText1(text1 : String): String {
     val decode2_5 = text1.map { char -> char + 1 }.joinToString("")
     val decode2_4 = decode2_5.replace('5', 's')
@@ -20,7 +25,7 @@ fun decodeText2(text2 : String): String {
     val decode3_2 = decode3_3.map { char -> char - 4 }.joinToString("")
     val decode3_1 = decode3_2.replace(oldChar = '_', newChar = ' ')
     return decode3_1
-}
-//fun shiftSymbol(text : String , printinfo: ): String {
 
-    //val schift1 = decodeText1() }
+}
+
+
