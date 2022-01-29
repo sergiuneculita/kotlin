9faaad7400ed36f01e277d3020fc9fe647ef0b25
sergiuneculita <199707adriana>
2022-01-29 19:29:14 +0100

    I created the program that finds the number in the Fibonacci sequence

--- 05_ConditionalExpressions_Loops/Homework2/Homework2.kt
+++ 05_ConditionalExpressions_Loops/Homework2/Homework2.kt
@@ -1,12 +1,23 @@
 fun main(){
-    val n = readLine()?.toIntOrNull()?:return
-    if (n < 0) {
-        fibona(n)
-    }
-    else{
-        println(" no corect number $n ")
-
-    }
-    while (n < 0){}
+    Check() }
+fun Check(){
+    println("Enter the number: ")
+    val n: Long = readLine()?.toLongOrNull()?:return
+    return if (n <= 0){
+        println("The number is not part of the Fibonacci sequence")
+        Check()
+    }else{calculate(n)
+        Check()} }
+fun calculate(n: Long){
+    var i: Long = 0
+    var fibSum = 0
+    var fib1 = 0
+    var fib2 = 1
+    while (i < n - 2){
+        fibSum = fib1 + fib2
+        fib1 = fib2
+        fib2 = fibSum
+        i++ }
+    println("The Fibonacci number is $fibSum" )
+    return
 }
-fun fibona(num: Int){
